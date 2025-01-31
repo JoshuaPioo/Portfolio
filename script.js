61365fea705970dd9ebd6e2c61dc33f0a1413dab
JoshuaPioo <joshuatablon92@gmail.com>
2025-01-31 16:11:28 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const menuToggle = document.querySelector(".menu-toggle");
-    const navMenu = document.querySelector(".nav-menu");
+var typing = new Typed(".typing", {
+    strings: ["Frontend", "Editor", "Designer"],
+    typeSpeed: 100,
+    backSpeed: 50,
+    loop: true
+});
 
-    menuToggle.addEventListener("click", function () {
-        navMenu.classList.toggle("active");
+document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const navMenu = document.querySelector('.nav-menu');
+
+    menuToggle.addEventListener('click', () => {
+        navMenu.classList.toggle('active');
+    });
+
+    document.addEventListener('click', (e) => {
+        if (!e.target.closest('.header')) {
+            navMenu.classList.remove('active');
+        }
     });
 });
