5209bbc1ff110ac916d8da4aa0dab44fb4feacbf
JoshuaPioo <joshuatablon92@gmail.com>
2025-09-25 20:05:57 +0800

    Fix case-sensitive imports for Vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
 import Home from "./sections/Home";
 import About from "./sections/About";
 import Projects from "./sections/Projects";
